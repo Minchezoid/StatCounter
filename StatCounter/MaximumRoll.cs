a49dac1f71dfebf339e25a71390c5ac5d4d149d6
microwaverr <129300206+microwaverr@users.noreply.github.com>
2023-05-05 23:20:17 +0300

    Update MaximumRoll.cs

--- StatCounter/MaximumRoll.cs
+++ StatCounter/MaximumRoll.cs
@@ -8,12 +8,12 @@ namespace StatCounter
 {
     class MaximumRoll
     {
-        public const double CRIT_DMG = 7.8;
+        public const double CRIT_DMG = 7.78;
         public const double CRIT_RATE = 3.9;
         public const double ELEMENTAL_MASTERY = 23.4;
-        public const double ATK_PERCENT = 5.9;
+        public const double ATK_PERCENT = 5.84;
         public const double ENERGY_RECHARGE = 6.5;
-        public const double HP_PERCENT = 5.9;
+        public const double HP_PERCENT = 5.84;
         public const double DEF_PERCENT = 7.3;
     }
 }
